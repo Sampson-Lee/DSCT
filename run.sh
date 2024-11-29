847ca27da9f0b7402751d75cc561e4e7c97573be
Sampson-Lee <li.xin.peng@qq.com>
2024-11-29 17:44:47 -0600

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # emotic training scripts
-YOUR_PATH=./
+YOUR_DATA_PATH=./
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch \
     --nproc_per_node=8 \
     --master_port=29507 \
@@ -10,34 +10,36 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch \
     --model=deformable_transformer \
     --batch_size=4 \
     --cls_loss_coef=5 \
-    --data_path=$YOUR_PATH/EMOTIC/images/ \
-    --output_dir=$YOUR_PATH/EMOTIC/checkpoints \
+    --data_path=$YOUR_DATA_PATH/EMOTIC/images/ \
+    --output_dir=$YOUR_DATA_PATH/EMOTIC/checkpoints \
     --epochs=50 \
     --lr_drop=40 \
     --num_queries=4;
 
 # emotic testing scripts
-DATA_PATH=./
-MODEL_PATH=./
+YOUR_DATA_PATH=./
+YOUR_MODEL_PATH=./
 CUDA_VISIBLE_DEVICES=1 python test.py \
         --dataset_file=emotic \
         --detr=deformable_detr_dsct \
         --model=deformable_transformer \
         --num_queries=4 \
         --binary_flag=1 \
-        --data_path=$DATA_PATH/EMOTIC/images/ \
-        --json_path=$DATA_PATH/EMOTIC/annotations/test_bi.json \
-        --resume=$MODEL_PATH/checkpoint.pth;
+        --data_path=$YOUR_DATA_PATH/EMOTIC/images/ \
+        --json_path=$YOUR_DATA_PATH/EMOTIC/annotations/test_bi.json \
+        --resume=$YOUR_MODEL_PATH/checkpoint.pth;
 
 # emotic visualization scripts
-DATA_PATH=./
-MODEL_PATH=./
+YOUR_DATA_PATH=./
+YOUR_MODEL_PATH=./
 CUDA_VISIBLE_DEVICES=7 python vis.py \
         --dataset_file=emotic \
         --num_queries=4 \
         --binary_flag=1 \
         --model=deformable_transformer \
         --detr=deformable_detr_dsct \
-        --data_path=$DATA_PATH/EMOTIC/images/ \
-        --json_path=$DATA_PATH/EMOTIC/annotations/test_bi.json \
-        --resume=$MODEL_PATH/checkpoint.pth;
+        --data_path=$YOUR_DATA_PATH/EMOTIC/images/ \
+        --json_path=$YOUR_DATA_PATH/EMOTIC/annotations/test_bi.json \
+        --resume=$YOUR_MODEL_PATH/checkpoint.pth;
+
+# for caer-s, please set binary_flag as 0
